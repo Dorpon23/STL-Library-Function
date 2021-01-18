fe7e28d9f86f5f2a8a892fc34056a76173f0de64
unknown <dorpon_se@yahoo.com>
2021-01-18 17:32:23 +0600

    vector

---
+++ Vector/1.vector.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> vec;
+
+    for(int i=0;i <64;i++){
+
+        vec.push_back(i);
+        cout<<" size:"<<vec.size()<<" Capcity:"<<vec.capacity()<<endl;
+
+    }
+
+}
